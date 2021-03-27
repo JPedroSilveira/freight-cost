82dcafef6371bdff67f4389bd500b6c2abb0371a
João Pedro <jpedross1999@gmail.com>
2021-03-27 18:11:25 -0300

    Feat(Logo): Definição de novo logo

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Main: React.FC = () => {
+const App: React.FC = () => {
     return (
         <div className='main'>
             
@@ -8,4 +8,4 @@ const Main: React.FC = () => {
     )
 }
 
-export default Main
+export default App
