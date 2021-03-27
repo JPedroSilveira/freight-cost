ef8e87771b2095bdc9227353624703a3ad8755d8
João Pedro <jpedross1999@gmail.com>
2021-03-27 19:14:18 -0300

    Feat(UI): Criação da base de roteamento

---
+++ src/view/main/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/header'
+
+const Main: React.FC = () => {
+    return (
+        <div className='main'>
+            <Header />
+        </div>
+    )
+}
+
+export default Main
